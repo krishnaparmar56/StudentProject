8364c141f30bbf75f51492777758673c48193895
Krishna <krishnaparmar567@gmail.com>
2018-11-03 13:01:26 +0530

    Removed unwanted code from country seeder

--- database/migrations/2018_11_03_070241_create_country_table.php
+++ database/migrations/2018_11_03_070241_create_country_table.php
@@ -13,22 +13,12 @@ class CreateCountryTable extends Migration
      */
     public function up()
     {
-        /*
-CREATE TABLE IF NOT EXISTS `countries` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sortname` varchar(3) NOT NULL,
-  `name` varchar(150) NOT NULL,
-  `phonecode` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=249 ;
-        */
         Schema::create('country', function (Blueprint $table) {
             $table->increments('id');
             $table->string('sortname',3);
             $table->string('name',150);
             $table->integer('phonecode');
             $table->boolean('status')->default(0);
-            //$table->timestamps();
         });
     }
 
